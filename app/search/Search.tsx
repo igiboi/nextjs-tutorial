eb6e3bb1535bc857d12abd7d6f727cef8e9b90ff
Luigi Matias <igiboi26@gmail.com>
2023-01-03 15:23:06 -0500

    created a search bar

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client"; // converts it to client side
+
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 function Search() {
-  return <div>I am a search component</div>;
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/search/${search}`);
+  };
+
+  return (
+    //   to find element type {e => handleSearch}
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        id="search"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg"
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
